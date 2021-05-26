a83dd2f66cfc918bb36a2854c67bb4d0f7cce476
sathsarabandaraj <sathsarabjayasundara@gmail.com>
2021-05-26 09:29:30 +0530

    Back to default form border style

--- Le-Sa/formDashboard.Designer.cs
+++ Le-Sa/formDashboard.Designer.cs
@@ -1,4 +1,5 @@
-﻿namespace Le_Sa
+﻿
+namespace Le_Sa
 {
     partial class formDashboard
     {
@@ -28,44 +29,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pnlTitleBar = new System.Windows.Forms.Panel();
             this.pnlDesktop = new System.Windows.Forms.Panel();
             this.pnlUser = new System.Windows.Forms.Panel();
             this.lblUserName = new System.Windows.Forms.Label();
+            this.pbUserPic = new System.Windows.Forms.PictureBox();
             this.pnlMenu = new System.Windows.Forms.Panel();
             this.ibtnSettings = new FontAwesome.Sharp.IconButton();
             this.ibtnInfo = new FontAwesome.Sharp.IconButton();
             this.ibtnRports = new FontAwesome.Sharp.IconButton();
             this.ibtnDashboard = new FontAwesome.Sharp.IconButton();
-            this.pbUserPic = new System.Windows.Forms.PictureBox();
-            this.btnMinimize = new System.Windows.Forms.Button();
-            this.btnMaximize = new System.Windows.Forms.Button();
-            this.btnClose = new System.Windows.Forms.Button();
-            this.pnlTitleBar.SuspendLayout();
             this.pnlUser.SuspendLayout();
-            this.pnlMenu.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbUserPic)).BeginInit();
+            this.pnlMenu.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // pnlTitleBar
-            // 
-            this.pnlTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
-            this.pnlTitleBar.Controls.Add(this.btnMinimize);
-            this.pnlTitleBar.Controls.Add(this.btnMaximize);
-            this.pnlTitleBar.Controls.Add(this.btnClose);
-            this.pnlTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pnlTitleBar.Location = new System.Drawing.Point(0, 0);
-            this.pnlTitleBar.Name = "pnlTitleBar";
-            this.pnlTitleBar.Size = new System.Drawing.Size(950, 27);
-            this.pnlTitleBar.TabIndex = 1;
-            this.pnlTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlTitleBar_MouseDown);
             // 
             // pnlDesktop
             // 
             this.pnlDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlDesktop.Location = new System.Drawing.Point(186, 27);
+            this.pnlDesktop.Location = new System.Drawing.Point(186, 0);
             this.pnlDesktop.Name = "pnlDesktop";
-            this.pnlDesktop.Size = new System.Drawing.Size(764, 573);
+            this.pnlDesktop.Size = new System.Drawing.Size(748, 561);
             this.pnlDesktop.TabIndex = 2;
             // 
             // pnlUser
@@ -89,6 +72,17 @@
             this.lblUserName.TabIndex = 1;
             this.lblUserName.Text = "User Name";
             // 
+            // pbUserPic
+            // 
+            this.pbUserPic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pbUserPic.Image = global::Le_Sa.Properties.Resources.user_65px;
+            this.pbUserPic.Location = new System.Drawing.Point(60, 16);
+            this.pbUserPic.Name = "pbUserPic";
+            this.pbUserPic.Size = new System.Drawing.Size(67, 67);
+            this.pbUserPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbUserPic.TabIndex = 0;
+            this.pbUserPic.TabStop = false;
+            // 
             // pnlMenu
             // 
             this.pnlMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
@@ -98,9 +92,9 @@
             this.pnlMenu.Controls.Add(this.ibtnDashboard);
             this.pnlMenu.Controls.Add(this.pnlUser);
             this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Left;
-            this.pnlMenu.Location = new System.Drawing.Point(0, 27);
+            this.pnlMenu.Location = new System.Drawing.Point(0, 0);
             this.pnlMenu.Name = "pnlMenu";
-            this.pnlMenu.Size = new System.Drawing.Size(186, 573);
+            this.pnlMenu.Size = new System.Drawing.Size(186, 561);
             this.pnlMenu.TabIndex = 0;
             // 
             // ibtnSettings
@@ -116,7 +110,7 @@
             this.ibtnSettings.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.ibtnSettings.IconSize = 30;
             this.ibtnSettings.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
-            this.ibtnSettings.Location = new System.Drawing.Point(0, 533);
+            this.ibtnSettings.Location = new System.Drawing.Point(0, 521);
             this.ibtnSettings.Name = "ibtnSettings";
             this.ibtnSettings.Size = new System.Drawing.Size(186, 40);
             this.ibtnSettings.TabIndex = 5;
@@ -128,6 +122,7 @@
             // 
             this.ibtnInfo.Dock = System.Windows.Forms.DockStyle.Top;
             this.ibtnInfo.FlatAppearance.BorderSize = 0;
+            this.ibtnInfo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.ibtnInfo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ibtnInfo.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ibtnInfo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
@@ -148,6 +143,7 @@
             // 
             this.ibtnRports.Dock = System.Windows.Forms.DockStyle.Top;
             this.ibtnRports.FlatAppearance.BorderSize = 0;
+            this.ibtnRports.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.ibtnRports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ibtnRports.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ibtnRports.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
@@ -169,6 +165,7 @@
             this.ibtnDashboard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
             this.ibtnDashboard.Dock = System.Windows.Forms.DockStyle.Top;
             this.ibtnDashboard.FlatAppearance.BorderSize = 0;
+            this.ibtnDashboard.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.ibtnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.ibtnDashboard.Font = new System.Drawing.Font("Nirmala UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ibtnDashboard.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(152)))), ((int)(((byte)(219)))));
@@ -185,83 +182,6 @@
             this.ibtnDashboard.UseVisualStyleBackColor = false;
             this.ibtnDashboard.Click += new System.EventHandler(this.ibtnDashboard_Click);
             // 
-            // pbUserPic
-            // 
-            this.pbUserPic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.pbUserPic.Image = global::Le_Sa.Properties.Resources.user_65px;
-            this.pbUserPic.Location = new System.Drawing.Point(60, 16);
-            this.pbUserPic.Name = "pbUserPic";
-            this.pbUserPic.Size = new System.Drawing.Size(67, 67);
-            this.pbUserPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pbUserPic.TabIndex = 0;
-            this.pbUserPic.TabStop = false;
-            // 
-            // btnMinimize
-            // 
-            this.btnMinimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
-            this.btnMinimize.BackgroundImage = global::Le_Sa.Properties.Resources.filled_minimize_20px;
-            this.btnMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.btnMinimize.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnMinimize.FlatAppearance.BorderSize = 0;
-            this.btnMinimize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
-            this.btnMinimize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
-            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMinimize.ForeColor = System.Drawing.Color.Transparent;
-            this.btnMinimize.Location = new System.Drawing.Point(869, 0);
-            this.btnMinimize.Name = "btnMinimize";
-            this.btnMinimize.Size = new System.Drawing.Size(27, 27);
-            this.btnMinimize.TabIndex = 2;
-            this.btnMinimize.TextAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.btnMinimize.UseVisualStyleBackColor = false;
-            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
-            this.btnMinimize.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnMinimize_MouseDown);
-            this.btnMinimize.MouseLeave += new System.EventHandler(this.btnMinimize_MouseLeave);
-            this.btnMinimize.MouseHover += new System.EventHandler(this.btnMinimize_MouseHover);
-            // 
-            // btnMaximize
-            // 
-            this.btnMaximize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
-            this.btnMaximize.BackgroundImage = global::Le_Sa.Properties.Resources.maximize_filled_20px;
-            this.btnMaximize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.btnMaximize.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnMaximize.FlatAppearance.BorderSize = 0;
-            this.btnMaximize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
-            this.btnMaximize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
-            this.btnMaximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMaximize.ForeColor = System.Drawing.Color.Transparent;
-            this.btnMaximize.Location = new System.Drawing.Point(896, 0);
-            this.btnMaximize.Name = "btnMaximize";
-            this.btnMaximize.Size = new System.Drawing.Size(27, 27);
-            this.btnMaximize.TabIndex = 1;
-            this.btnMaximize.TextAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.btnMaximize.UseVisualStyleBackColor = false;
-            this.btnMaximize.Click += new System.EventHandler(this.btnMaximize_Click);
-            this.btnMaximize.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnMaximize_MouseDown);
-            this.btnMaximize.MouseLeave += new System.EventHandler(this.btnMaximize_MouseLeave);
-            this.btnMaximize.MouseHover += new System.EventHandler(this.btnMaximize_MouseHover);
-            // 
-            // btnClose
-            // 
-            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
-            this.btnClose.BackgroundImage = global::Le_Sa.Properties.Resources.filled_close_20px;
-            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.btnClose.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btnClose.FlatAppearance.BorderSize = 0;
-            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
-            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
-            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnClose.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
-            this.btnClose.Location = new System.Drawing.Point(923, 0);
-            this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(27, 27);
-            this.btnClose.TabIndex = 0;
-            this.btnClose.TextAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.btnClose.UseVisualStyleBackColor = false;
-            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
-            this.btnClose.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnClose_MouseDown);
-            this.btnClose.MouseLeave += new System.EventHandler(this.btnClose_MouseLeave);
-            this.btnClose.MouseHover += new System.EventHandler(this.btnClose_MouseHover);
-            // 
             // formDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -269,29 +189,22 @@
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(40)))), ((int)(((byte)(74)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(950, 600);
+            this.ClientSize = new System.Drawing.Size(934, 561);
             this.Controls.Add(this.pnlDesktop);
             this.Controls.Add(this.pnlMenu);
-            this.Controls.Add(this.pnlTitleBar);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MinimumSize = new System.Drawing.Size(950, 600);
             this.Name = "formDashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.pnlTitleBar.ResumeLayout(false);
             this.pnlUser.ResumeLayout(false);
             this.pnlUser.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbUserPic)).EndInit();
             this.pnlMenu.ResumeLayout(false);
             this.pnlMenu.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbUserPic)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.Panel pnlTitleBar;
-        private System.Windows.Forms.Button btnClose;
-        private System.Windows.Forms.Button btnMinimize;
-        private System.Windows.Forms.Button btnMaximize;
         private System.Windows.Forms.Panel pnlDesktop;
         private System.Windows.Forms.Panel pnlUser;
         private System.Windows.Forms.Label lblUserName;
--- Le-Sa/formDashboard.cs
+++ Le-Sa/formDashboard.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FontAwesome.Sharp;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace Le_Sa
@@ -17,174 +16,66 @@ namespace Le_Sa
 
         private IconButton currentBtn;
 
+
         public formDashboard()
         {
             InitializeComponent();
         }
 
-        private struct RGBColors
-        {
-            public static Color desktopBackDefault = Color.FromArgb(34, 40, 74);
-            public static Color normal = Color.FromArgb(52, 152, 219);
-            public static Color settings = Color.FromArgb(231, 76, 60);
-            public static Color menuBackDefault = Color.FromArgb(24, 30, 54);
-        }
 
-        #region Side Menu
-        private void ActiveButton(object senderBtn, Color color)
-        {
-            if (senderBtn != null)
-            {
-                DisableButton();
-                //Button customization
-                currentBtn = (IconButton)senderBtn;
-                currentBtn.BackColor = RGBColors.desktopBackDefault;
-                currentBtn.IconColor = color;
-                currentBtn.ForeColor = color;
-
-            }
-        }
-
-        private void DisableButton()
-        {
-            if (currentBtn != null)
-            {
-                //Button customization to default
-                currentBtn.BackColor = RGBColors.menuBackDefault;
-                currentBtn.IconColor = RGBColors.normal;
-                currentBtn.ForeColor = RGBColors.normal;
-            }
-        }
-
-        private void ibtnDashboard_Click(object sender, EventArgs e)
-        {
-            ActiveButton(sender, RGBColors.normal);
-        }
-
-        private void ibtnRports_Click(object sender, EventArgs e)
-        {
-            ActiveButton(sender, RGBColors.normal);
-        }
-
-        private void ibtnInfo_Click(object sender, EventArgs e)
-        {
-            ActiveButton(sender, RGBColors.normal);
-        }
-
-        private void ibtnSettings_Click(object sender, EventArgs e)
-        {
-            ActiveButton(sender, RGBColors.settings);
-        }
-        #endregion
-
-        #region Resizability 
-        //Resizable form
-        private const int cGrip = 16;
-
-        protected override void WndProc(ref Message m)
-        {
-            if (m.Msg == 0x84)
-            {
-                Point pos = new Point(m.LParam.ToInt32());
-                pos = this.PointToScreen(pos);
-                if (pos.X >= ClientSize.Width - cGrip && pos.Y >= ClientSize.Height - cGrip)
-                {
-                    m.Result = (IntPtr)17;
-                    return;
-                }
-            }
-            base.WndProc(ref m);
-        }
-        //Resizable form end
-        #endregion
-
-        #region Draggability
-        //Draggable form
-        public const int WM_NCLBUTTONDOWN = 0xA1;
-        public const int HT_CAPTION = 0x2;
-
-        [DllImportAttribute("user32.dll")]
-        public static extern int SendMessage(IntPtr hWnd,
-                         int Msg, int wParam, int lParam);
-        [DllImportAttribute("user32.dll")]
-        public static extern bool ReleaseCapture();
-
-        private void pnlTitleBar_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                ReleaseCapture();
-                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
-            }
-        }
-        //Draggable form end
-        #endregion
-
-        #region Ttitle bar control buttons
-
-        private void btnClose_Click(object sender, EventArgs e)
-        {
-            Application.Exit();  //close event
-        }
-        private void btnClose_MouseDown(object sender, MouseEventArgs e)
-        {
-            btnClose.BackgroundImage = Properties.Resources.close_click20px;
-        }
-        private void btnClose_MouseHover(object sender, EventArgs e)
-        {
-            btnClose.BackgroundImage = Properties.Resources.close_20px;
-        }
-        private void btnClose_MouseLeave(object sender, EventArgs e)
-        {
-            btnClose.BackgroundImage = Properties.Resources.filled_close_20px;
-        }
-
-        private void btnMinimize_Click(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Minimized; //minimize events
-        }
-
-        private void btnMinimize_MouseDown(object sender, MouseEventArgs e)
-        {
-            btnMinimize.BackgroundImage = Properties.Resources.minimize_click20px;
-        }
-
-        private void btnMinimize_MouseHover(object sender, EventArgs e)
-        {
-            btnMinimize.BackgroundImage = Properties.Resources.minimize_20px;
-        }
-
-        private void btnMinimize_MouseLeave(object sender, EventArgs e)
-        {
-            btnMinimize.BackgroundImage = Properties.Resources.filled_minimize_20px;
-        }
-
-        private void btnMaximize_Click(object sender, EventArgs e)
-        {
-            if (this.WindowState == FormWindowState.Normal) //check window state
-            {
-                this.MaximumSize = Screen.PrimaryScreen.WorkingArea.Size;
-                this.WindowState = FormWindowState.Maximized;
-            }
-            else
-            {
-                this.WindowState = FormWindowState.Normal;
-            }
-        }
-
-        private void btnMaximize_MouseDown(object sender, MouseEventArgs e)
-        {
-            btnMaximize.BackgroundImage = Properties.Resources.maximize_click_20px;
-        }
-        private void btnMaximize_MouseHover(object sender, EventArgs e)
-        {
-            btnMaximize.BackgroundImage = Properties.Resources.maximize_20px;
-        }
-        private void btnMaximize_MouseLeave(object sender, EventArgs e)
-        {
-            btnMaximize.BackgroundImage = Properties.Resources.maximize_filled_20px;
-        }
-        #endregion
-
+    private struct RGBColors
+    {
+        public static Color desktopBackDefault = Color.FromArgb(34, 40, 74);
+        public static Color normal = Color.FromArgb(52, 152, 219);
+        public static Color settings = Color.FromArgb(231, 76, 60);
+        public static Color menuBackDefault = Color.FromArgb(24, 30, 54);
     }
+
+    #region Side Menu
+    private void ActiveButton(object senderBtn, Color color)
+    {
+        if (senderBtn != null)
+        {
+            DisableButton();
+            //Button customization
+            currentBtn = (IconButton)senderBtn;
+            currentBtn.BackColor = RGBColors.desktopBackDefault;
+            currentBtn.IconColor = color;
+            currentBtn.ForeColor = color;
+
+        }
+    }
+
+    private void DisableButton()
+    {
+        if (currentBtn != null)
+        {
+            //Button customization to default
+            currentBtn.BackColor = RGBColors.menuBackDefault;
+            currentBtn.IconColor = RGBColors.normal;
+            currentBtn.ForeColor = RGBColors.normal;
+        }
+    }
+
+    private void ibtnDashboard_Click(object sender, EventArgs e)
+    {
+        ActiveButton(sender, RGBColors.normal);
+    }
+
+    private void ibtnRports_Click(object sender, EventArgs e)
+    {
+        ActiveButton(sender, RGBColors.normal);
+    }
+
+    private void ibtnInfo_Click(object sender, EventArgs e)
+    {
+        ActiveButton(sender, RGBColors.normal);
+    }
+
+    private void ibtnSettings_Click(object sender, EventArgs e)
+    {
+        ActiveButton(sender, RGBColors.settings);
+    }
+    #endregion
+}
 }
