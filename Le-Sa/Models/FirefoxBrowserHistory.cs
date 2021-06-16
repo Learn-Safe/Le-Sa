eb7b3c49e3f2a93769534f86cf55fc36b51851b7
sathsarabandaraj <sathsarabjayasundara@gmail.com>
2021-06-16 17:16:35 +0530

    Add 'Visit Count' into the history table and set the table in descending order

--- Le-Sa/Models/FirefoxBrowserHistory.cs
+++ Le-Sa/Models/FirefoxBrowserHistory.cs
@@ -12,8 +12,8 @@ namespace Le_Sa.Models
         public FirefoxBrowserHistory()
         {
             path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Le-Sa\Data\FData\FHis\places.sqlite";
-            query = @"SELECT url AS URL, title AS TITLE, time(visit_date / 1000000, 'unixepoch') AS TIME, date(visit_date / 1000000, 'unixepoch') AS DATE 
-                      FROM moz_historyvisits, moz_places WHERE moz_historyvisits.place_id=moz_places.id ORDER BY visit_date";
+            query = @"SELECT url AS URL, title AS TITLE, visit_count AS 'VISIT COUNT', time(visit_date / 1000000, 'unixepoch') AS TIME, date(visit_date / 1000000, 'unixepoch') AS DATE 
+                      FROM moz_historyvisits, moz_places WHERE moz_historyvisits.place_id=moz_places.id ORDER BY visit_date DESC";
             name = "Firefox";
         }
     }
