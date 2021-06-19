c844624503db8fef016db62b09c13fc19f209a95
sathsarabandaraj <sathsarabjayasundara@gmail.com>
2021-06-19 11:10:07 +0530

    Minor UI changes in formHistory

--- Le-Sa/History/formHistory.Designer.cs
+++ Le-Sa/History/formHistory.Designer.cs
@@ -30,12 +30,12 @@ namespace Le_Sa.History
         private void InitializeComponent()
         {
             this.pnlDashboard = new System.Windows.Forms.Panel();
-            this.pnlDesktop = new System.Windows.Forms.Panel();
-            this.dgvBrowserHistory = new System.Windows.Forms.DataGridView();
             this.crBtnOpera = new Le_Sa.CustRoundedButton();
             this.crBtnEdge = new Le_Sa.CustRoundedButton();
             this.crBtnFirefox = new Le_Sa.CustRoundedButton();
             this.crBtnChrome = new Le_Sa.CustRoundedButton();
+            this.pnlDesktop = new System.Windows.Forms.Panel();
+            this.dgvBrowserHistory = new System.Windows.Forms.DataGridView();
             this.pnlDashboard.SuspendLayout();
             this.pnlDesktop.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvBrowserHistory)).BeginInit();
@@ -52,8 +52,88 @@ namespace Le_Sa.History
             this.pnlDashboard.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlDashboard.Location = new System.Drawing.Point(0, 0);
             this.pnlDashboard.Name = "pnlDashboard";
-            this.pnlDashboard.Size = new System.Drawing.Size(694, 50);
+            this.pnlDashboard.Size = new System.Drawing.Size(694, 40);
             this.pnlDashboard.TabIndex = 1;
+            // 
+            // crBtnOpera
+            // 
+            this.crBtnOpera.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
+            this.crBtnOpera.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
+            this.crBtnOpera.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(27)))), ((int)(((byte)(45)))));
+            this.crBtnOpera.BorderRadius = 20;
+            this.crBtnOpera.BorderSize = 0;
+            this.crBtnOpera.FlatAppearance.BorderSize = 0;
+            this.crBtnOpera.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.crBtnOpera.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.crBtnOpera.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
+            this.crBtnOpera.Location = new System.Drawing.Point(273, 5);
+            this.crBtnOpera.Name = "crBtnOpera";
+            this.crBtnOpera.Size = new System.Drawing.Size(80, 30);
+            this.crBtnOpera.TabIndex = 3;
+            this.crBtnOpera.Text = "Opera";
+            this.crBtnOpera.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
+            this.crBtnOpera.UseVisualStyleBackColor = false;
+            this.crBtnOpera.Click += new System.EventHandler(this.crBtnOpera_Click);
+            // 
+            // crBtnEdge
+            // 
+            this.crBtnEdge.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
+            this.crBtnEdge.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
+            this.crBtnEdge.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(76)))), ((int)(((byte)(141)))));
+            this.crBtnEdge.BorderRadius = 20;
+            this.crBtnEdge.BorderSize = 0;
+            this.crBtnEdge.FlatAppearance.BorderSize = 0;
+            this.crBtnEdge.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.crBtnEdge.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.crBtnEdge.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
+            this.crBtnEdge.Location = new System.Drawing.Point(187, 5);
+            this.crBtnEdge.Name = "crBtnEdge";
+            this.crBtnEdge.Size = new System.Drawing.Size(80, 30);
+            this.crBtnEdge.TabIndex = 2;
+            this.crBtnEdge.Text = "Edge";
+            this.crBtnEdge.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
+            this.crBtnEdge.UseVisualStyleBackColor = false;
+            this.crBtnEdge.Click += new System.EventHandler(this.crBtnEdge_Click);
+            // 
+            // crBtnFirefox
+            // 
+            this.crBtnFirefox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
+            this.crBtnFirefox.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
+            this.crBtnFirefox.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(48)))), ((int)(((byte)(82)))));
+            this.crBtnFirefox.BorderRadius = 20;
+            this.crBtnFirefox.BorderSize = 0;
+            this.crBtnFirefox.FlatAppearance.BorderSize = 0;
+            this.crBtnFirefox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.crBtnFirefox.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.crBtnFirefox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
+            this.crBtnFirefox.Location = new System.Drawing.Point(101, 5);
+            this.crBtnFirefox.Name = "crBtnFirefox";
+            this.crBtnFirefox.Size = new System.Drawing.Size(80, 30);
+            this.crBtnFirefox.TabIndex = 1;
+            this.crBtnFirefox.Text = "Firefox";
+            this.crBtnFirefox.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
+            this.crBtnFirefox.UseVisualStyleBackColor = false;
+            this.crBtnFirefox.Click += new System.EventHandler(this.crBtnFirefox_Click);
+            // 
+            // crBtnChrome
+            // 
+            this.crBtnChrome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
+            this.crBtnChrome.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
+            this.crBtnChrome.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(91)))), ((int)(((byte)(234)))));
+            this.crBtnChrome.BorderRadius = 20;
+            this.crBtnChrome.BorderSize = 0;
+            this.crBtnChrome.FlatAppearance.BorderSize = 0;
+            this.crBtnChrome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.crBtnChrome.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.crBtnChrome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
+            this.crBtnChrome.Location = new System.Drawing.Point(5, 5);
+            this.crBtnChrome.Name = "crBtnChrome";
+            this.crBtnChrome.Size = new System.Drawing.Size(90, 30);
+            this.crBtnChrome.TabIndex = 0;
+            this.crBtnChrome.Text = "Chrome";
+            this.crBtnChrome.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
+            this.crBtnChrome.UseVisualStyleBackColor = false;
+            this.crBtnChrome.Click += new System.EventHandler(this.crBtnChrome_Click);
             // 
             // pnlDesktop
             // 
@@ -61,9 +141,9 @@ namespace Le_Sa.History
             this.pnlDesktop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(39)))), ((int)(((byte)(75)))));
             this.pnlDesktop.Controls.Add(this.dgvBrowserHistory);
             this.pnlDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlDesktop.Location = new System.Drawing.Point(0, 50);
+            this.pnlDesktop.Location = new System.Drawing.Point(0, 40);
             this.pnlDesktop.Name = "pnlDesktop";
-            this.pnlDesktop.Size = new System.Drawing.Size(694, 511);
+            this.pnlDesktop.Size = new System.Drawing.Size(694, 521);
             this.pnlDesktop.TabIndex = 2;
             // 
             // dgvBrowserHistory
@@ -78,88 +158,8 @@ namespace Le_Sa.History
             this.dgvBrowserHistory.Location = new System.Drawing.Point(0, 0);
             this.dgvBrowserHistory.Name = "dgvBrowserHistory";
             this.dgvBrowserHistory.RowHeadersVisible = false;
-            this.dgvBrowserHistory.Size = new System.Drawing.Size(694, 511);
+            this.dgvBrowserHistory.Size = new System.Drawing.Size(694, 521);
             this.dgvBrowserHistory.TabIndex = 0;
-            // 
-            // crBtnOpera
-            // 
-            this.crBtnOpera.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
-            this.crBtnOpera.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
-            this.crBtnOpera.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(27)))), ((int)(((byte)(45)))));
-            this.crBtnOpera.BorderRadius = 18;
-            this.crBtnOpera.BorderSize = 0;
-            this.crBtnOpera.FlatAppearance.BorderSize = 0;
-            this.crBtnOpera.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.crBtnOpera.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.crBtnOpera.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
-            this.crBtnOpera.Location = new System.Drawing.Point(308, 5);
-            this.crBtnOpera.Name = "crBtnOpera";
-            this.crBtnOpera.Size = new System.Drawing.Size(95, 40);
-            this.crBtnOpera.TabIndex = 3;
-            this.crBtnOpera.Text = "Opera";
-            this.crBtnOpera.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
-            this.crBtnOpera.UseVisualStyleBackColor = false;
-            this.crBtnOpera.Click += new System.EventHandler(this.crBtnOpera_Click);
-            // 
-            // crBtnEdge
-            // 
-            this.crBtnEdge.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
-            this.crBtnEdge.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
-            this.crBtnEdge.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(76)))), ((int)(((byte)(141)))));
-            this.crBtnEdge.BorderRadius = 18;
-            this.crBtnEdge.BorderSize = 0;
-            this.crBtnEdge.FlatAppearance.BorderSize = 0;
-            this.crBtnEdge.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.crBtnEdge.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.crBtnEdge.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
-            this.crBtnEdge.Location = new System.Drawing.Point(207, 5);
-            this.crBtnEdge.Name = "crBtnEdge";
-            this.crBtnEdge.Size = new System.Drawing.Size(95, 40);
-            this.crBtnEdge.TabIndex = 2;
-            this.crBtnEdge.Text = "Edge";
-            this.crBtnEdge.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
-            this.crBtnEdge.UseVisualStyleBackColor = false;
-            this.crBtnEdge.Click += new System.EventHandler(this.crBtnEdge_Click);
-            // 
-            // crBtnFirefox
-            // 
-            this.crBtnFirefox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
-            this.crBtnFirefox.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
-            this.crBtnFirefox.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(48)))), ((int)(((byte)(82)))));
-            this.crBtnFirefox.BorderRadius = 18;
-            this.crBtnFirefox.BorderSize = 0;
-            this.crBtnFirefox.FlatAppearance.BorderSize = 0;
-            this.crBtnFirefox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.crBtnFirefox.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.crBtnFirefox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
-            this.crBtnFirefox.Location = new System.Drawing.Point(106, 5);
-            this.crBtnFirefox.Name = "crBtnFirefox";
-            this.crBtnFirefox.Size = new System.Drawing.Size(95, 40);
-            this.crBtnFirefox.TabIndex = 1;
-            this.crBtnFirefox.Text = "Firefox";
-            this.crBtnFirefox.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
-            this.crBtnFirefox.UseVisualStyleBackColor = false;
-            this.crBtnFirefox.Click += new System.EventHandler(this.crBtnFirefox_Click);
-            // 
-            // crBtnChrome
-            // 
-            this.crBtnChrome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
-            this.crBtnChrome.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(27)))), ((int)(((byte)(56)))));
-            this.crBtnChrome.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(91)))), ((int)(((byte)(234)))));
-            this.crBtnChrome.BorderRadius = 18;
-            this.crBtnChrome.BorderSize = 0;
-            this.crBtnChrome.FlatAppearance.BorderSize = 0;
-            this.crBtnChrome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.crBtnChrome.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.crBtnChrome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
-            this.crBtnChrome.Location = new System.Drawing.Point(5, 5);
-            this.crBtnChrome.Name = "crBtnChrome";
-            this.crBtnChrome.Size = new System.Drawing.Size(95, 40);
-            this.crBtnChrome.TabIndex = 0;
-            this.crBtnChrome.Text = "Chrome";
-            this.crBtnChrome.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(211)))), ((int)(((byte)(213)))));
-            this.crBtnChrome.UseVisualStyleBackColor = false;
-            this.crBtnChrome.Click += new System.EventHandler(this.crBtnChrome_Click);
             // 
             // formHistory
             // 
