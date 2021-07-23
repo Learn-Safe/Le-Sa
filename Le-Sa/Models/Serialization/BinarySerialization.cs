95d7fa2c94d0c35d21fc8e0180a20923528ffa3e
Sathsara Bandara Jayasundara <sathsarabjayasundara@gmail.com>
2021-07-23 16:55:54 +0530

    Add new binary serialization model

---
+++ Le-Sa/Models/Serialization/BinarySerialization.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Le_Sa.Models.Serialization
+{
+    class BinarySerialization
+    {
+        public static void Serialize(object data, string filePath)
+        {
+            try
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                if (File.Exists(filePath))
+                {
+                    FileStream fileStream = new FileStream(filePath, FileMode.Append);
+                    bf.Serialize(fileStream, data);
+                    fileStream.Close();
+                }
+                else
+                {
+                    FileStream fileStream = new FileStream(filePath, FileMode.CreateNew);
+                    bf.Serialize(fileStream, data);
+                    fileStream.Close();
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static object Deserialize(string filePath)
+        {
+            object obj = null;
+            FileStream fileStream;
+            try
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                if (File.Exists(filePath))
+                {
+                    fileStream = File.OpenRead(filePath);
+                    bf.Deserialize(fileStream);
+                    fileStream.Close();
+                }
+                return obj;
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return obj;
+            }
+        }
+    }
+}
